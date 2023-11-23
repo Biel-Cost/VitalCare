243aa0b46c20baecef4eccde49f521a625179b64
Biel-Cost <jorel180703@gmail.com>
2023-11-23 19:39:41 -0300

    Adjusts

--- vital-care/src/components/Header/Header.tsx
+++ vital-care/src/components/Header/Header.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import { Button } from "../Button/Button";
 import Logo from "@/app/icon.svg";
 import styled from './Header.module.css'
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
     <header className={styled.headerContainer}>
       <div className={styled.headerTitle}>
@@ -13,9 +16,10 @@ export const Header = () => {
         <h1>VitalCare</h1>
       </div>
       <div className={styled.headerButtons}>
-        <Button onClick={() => {}}> Pulseira </Button>
-        <Button onClick={() => {}}> Login </Button>
-        <Button onClick={() => {}}> Cadastro </Button>
+        <Button onClick={() => {router.push("/")}}> Home </Button>
+        <Button onClick={() => {router.push("/pulseira")}}> Pulseira </Button>
+        <Button onClick={() => {router.push("/login")}}> Login </Button>
+        <Button onClick={() => {router.push("/cadastro")}}> Cadastro </Button>
       </div>
     </header>
   );
