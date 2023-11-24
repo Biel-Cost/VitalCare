fac0c5c425c141163889d266764cacce5578e171
19helena <helenamedeiros72109@gmail.com>
2023-11-24 00:07:46 -0300

    Created multiLabel component + health info page

--- vital-care/src/app/cadastro/informacoes-saude/page.tsx
+++ vital-care/src/app/cadastro/informacoes-saude/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { Button } from "@/components/Button/Button";
+import { useRouter } from "next/navigation";
+import styled from "./infoSaude.module.css";
+import { MultiLabel } from "@/components/MultiLabel/MultiLabel";
+
 export default function SaudeInfo() {
-    return('')
+  const router = useRouter();
+
+  return (
+    <div className={styled.infoSaudeContainer}>
+      <h1>Informações de Saúde</h1>
+      <div className={styled.infoSaudeForm}>
+        <div className={styled.infoSaudeLabels}>
+          <MultiLabel> Tem alergias? </MultiLabel>
+          <MultiLabel> Toma algum medicamento controlado? </MultiLabel>
+        </div>
+        <div className={styled.infoSaudeLabels}>
+          <MultiLabel> Já fez cirurgia? </MultiLabel>
+          <MultiLabel> Tem alguma condição médica pré-existente?</MultiLabel>
+        </div>
+      </div>
+      <div>
+        <Button onClick={() => {}}>Próximo</Button>
+      </div>
+    </div>
+  );
 }
